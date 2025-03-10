5c0f4066dc28f8a3185c3a2b2769af5988a3d51e
Dan Berglund <dan@fullbeaker.com>
2025-03-10 14:17:32 -0700

    remove hardcoded ARCH for docker repo

--- stage-coach2mqtt/stage-coach2mqtt/01-install-software/00-run.sh
+++ stage-coach2mqtt/stage-coach2mqtt/01-install-software/00-run.sh
@@ -4,7 +4,6 @@
 install -m 644 files/docker.asc "${ROOTFS_DIR}/etc/apt/keyrings/"
 install -m 644 files/docker.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 sed -i "s/RELEASE/${RELEASE}/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/docker.list"
-sed -i "s/ARCH/arm64/g" "${ROOTFS_DIR}/etc/apt/sources.list.d/docker.list"
 
 #AllStarLink repo for cockpit wifi plugin
 install -m 644 files/allstarlink.gpg "${ROOTFS_DIR}/etc/apt/keyrings/"
