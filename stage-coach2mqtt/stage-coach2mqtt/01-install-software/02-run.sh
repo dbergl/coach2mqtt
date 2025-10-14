8d26fcbd6320f3f1808ce82b2cf18b50300c32ed
Dan Berglund <dan@fullbeaker.com>
2025-10-13 22:40:24 -0700

    fix mosquitto.conf permissions, add clarification for hivemq tls mqtt url

--- stage-coach2mqtt/stage-coach2mqtt/01-install-software/02-run.sh
+++ stage-coach2mqtt/stage-coach2mqtt/01-install-software/02-run.sh
@@ -7,10 +7,10 @@ fi
 git clone --depth=1 --single-branch --branch main \
     https://github.com/dbergl/coach2mqtt.git "${ROOTFS_DIR}/opt/coach2mqtt"
 
-chown -R 1000:1000 "${ROOTFS_DIR}/opt/coach2mqtt"
-
 cp "${ROOTFS_DIR}/opt/coach2mqtt/configs/mosquitto/mosquitto.conf.example" \
     "${ROOTFS_DIR}/opt/coach2mqtt/configs/mosquitto/mosquitto.conf"
+
+chown -R 1000:1000 "${ROOTFS_DIR}/opt/coach2mqtt"
 
 #Use systemd to start service
 install -m 644 files/coach2mqtt.service "${ROOTFS_DIR}/lib/systemd/system/"
