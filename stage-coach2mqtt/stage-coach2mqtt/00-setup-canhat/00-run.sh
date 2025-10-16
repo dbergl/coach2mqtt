59751b86cf924923b7f2e7b9197845ad1c34355d
Dan Berglund <dan@fullbeaker.com>
2025-10-16 15:25:25 -0700

    Add support for Waveshare Can expansion board

--- stage-coach2mqtt/stage-coach2mqtt/00-setup-canhat/00-run.sh
+++ stage-coach2mqtt/stage-coach2mqtt/00-setup-canhat/00-run.sh
@@ -1,24 +1,13 @@
 #!/bin/bash -e
 
-if [ "${WAVESHARESKU}" == "27338" ]; then
-    # Update config.txt for Waveshare 2-Ch Can Hat+
-    # https://www.waveshare.com/wiki/2-CH_CAN_HAT+
-    sed '/^\[all\]/r'<(cat <<EOF
-    dtoverlay=spi1-3cs
-    dtoverlay=mcp2515,spi1-1,oscillator=16000000,interrupt=22
-    dtoverlay=mcp2515,spi1-2,oscillator=16000000,interrupt=13
-    EOF
-    ) -i -- "${ROOTFS_DIR}/boot/firmware/config.txt"
-elif [ "${WAVESHARESKU}" == "28164" ]; then
-    # Update config.txt for Waveshare Isolated RS232 / RS485 / CAN / CAN FD Expansion Board
-    # https://www.waveshare.com/product/raspberry-pi/hats/interface-power/rs232-rs485-can-board.htm
-    sed '/^\[all\]/r'<(cat <<EOF
-    dtoverlay=spi1-3cs
-    dtoverlay=mcp2515,spi1-1,oscillator=16000000,interrupt=22
-    dtoverlay=mcp2515,spi1-2,oscillator=16000000,interrupt=13
-    EOF
-    ) -i -- "${ROOTFS_DIR}/boot/firmware/config.txt"
-fi
+# Update config.txt for Waveshare 2-Ch Can Hat+
+# https://www.waveshare.com/wiki/2-CH_CAN_HAT+
+sed '/^\[all\]/r'<(cat <<EOF
+dtoverlay=spi1-3cs
+dtoverlay=mcp2515,spi1-1,oscillator=16000000,interrupt=22
+dtoverlay=mcp2515,spi1-2,oscillator=16000000,interrupt=13
+EOF
+) -i -- "${ROOTFS_DIR}/boot/firmware/config.txt"
 
 #Enable can modules
 install -v -m 644 files/can.conf "${ROOTFS_DIR}/etc/modules-load.d/can.conf"
