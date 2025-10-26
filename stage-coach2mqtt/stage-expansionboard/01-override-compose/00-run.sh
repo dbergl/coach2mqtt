91a6b03675ab8410e19acf7a509b70446103dabe
Dan Berglund <dan@fullbeaker.com>
2025-10-25 23:03:04 -0700

    chown 1000:1000 for docker-compose.override.yml

--- stage-coach2mqtt/stage-expansionboard/01-override-compose/00-run.sh
+++ stage-coach2mqtt/stage-expansionboard/01-override-compose/00-run.sh
@@ -2,4 +2,4 @@
 
 # Add docker-compose.override.yml
 # This overrides and adds configuration to use the expansion board rs485 interface
-install -v -m 644 -o ${FIRST_USER_NAME} -g ${FIRST_USER_NAME} files/docker-compose.override.yml ""${ROOTFS_DIR}/opt/coach2mqtt/
+install -v -m 644 -o 1000 -g 1000 files/docker-compose.override.yml ""${ROOTFS_DIR}/opt/coach2mqtt/
